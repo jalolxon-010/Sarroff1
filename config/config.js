7b24ecb90e7c52e4582e0373a729248ed4e08c47
jalolxon-010 <jalolxonraximov010gmail.com>
2026-03-28 15:28:17 +0500

    fix: database connection with ssl and url

--- config/config.js
+++ config/config.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+// Render'dagi DATABASE_URL dan foydalanish uchun ulanish
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Render bazalari uchun SSL shart
+    },
+  },
+});
 
-module.exports = sequelize; 
+module.exports = sequelize;
